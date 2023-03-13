3d7364b7cacac2608d91f14f69205ea8ebb3b3d3
ladc <72278371+WuFdev@users.noreply.github.com>
2023-03-13 16:14:40 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+import openai
+import sys
+openai.api_key = sys.argv[1]
+request = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": sys.argv[0]},
+    ]
+)
+print(request)
